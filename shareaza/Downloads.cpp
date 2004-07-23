045e8ebd5e42d9e2c7dddae55fb022ba63224fbf
mogthecat <mogthecat@3231ed4b-2112-0410-84e0-8c3944a40730>
2004-07-23 05:09:23 +0000

    Overloaded 'add' function for downloads
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@187 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/Downloads.cpp
+++ shareaza/Downloads.cpp
@@ -115,19 +115,11 @@ CDownload* CDownloads::Add(CQueryHit* pHit, BOOL bAddToHead)
 		theApp.Message( MSG_DOWNLOAD, IDS_DOWNLOAD_ADDED,
 			(LPCTSTR)pDownload->GetDisplayName(), pDownload->GetSourceCount() );
 	}
-
+	
 	pHit->m_bDownload = TRUE;
 	
 	DownloadGroups.Link( pDownload );
 	Transfers.StartThread();
-
-	if ( ( (pDownload->GetSourceCount() == 0 ) ||
-		   ( pDownload->m_bED2K && ! pDownload->m_bSHA1 )) &&
-		   (GetCount(TRUE) < Settings.Downloads.MaxFiles ) )
-	{
-		pDownload->SetStartTimer();
-	}
-	
 	
 	return pDownload;
 }
@@ -189,7 +181,6 @@ CDownload* CDownloads::Add(CMatchFile* pFile, BOOL bAddToHead)
 		   (GetCount(TRUE) < Settings.Downloads.MaxFiles ) )
 	{
 		pDownload->FindMoreSources();
-		pDownload->SetStartTimer();
 	}
 	
 	return pDownload;
@@ -284,13 +275,8 @@ CDownload* CDownloads::Add(CShareazaURL* pURL)
 	theApp.Message( MSG_DOWNLOAD, IDS_DOWNLOAD_ADDED,
 		(LPCTSTR)pDownload->GetDisplayName(), pDownload->GetSourceCount() );
 	
-	if( ( pDownload->m_bBTH && ( GetActiveTorrentCount() < Settings.BitTorrent.DownloadTorrents ) ) ||
-		( ! pDownload->m_bBTH && ( GetCount(TRUE) < Settings.Downloads.MaxFiles ) ) )
-	{
-		pDownload->SetStartTimer();
-		if ( pURL->m_nAction != CShareazaURL::uriSource )
-			pDownload->FindMoreSources();
-	}
+	if ( pURL->m_nAction != CShareazaURL::uriSource )
+		pDownload->FindMoreSources();
 	
 	DownloadGroups.Link( pDownload );
 	Transfers.StartThread();
@@ -375,23 +361,6 @@ int CDownloads::GetSeedCount() const
 		CDownload* pDownload = GetNext( pos );
 		
 		if ( pDownload->IsSeeding() )
-				nCount++;
-	}
-	
-	return nCount;
-}
-
-int CDownloads::GetActiveTorrentCount() const
-{
-	int nCount = 0;
-	
-	for ( POSITION pos = GetIterator() ; pos ; )
-	{
-		CDownload* pDownload = GetNext( pos );
-		
-		if ( pDownload->IsDownloading() && pDownload->m_bBTH &&
-			! pDownload->IsSeeding()	&& ! pDownload->IsCompleted() &&
-			! pDownload->IsMoving()		&& ! pDownload->IsPaused() )
 				nCount++;
 	}
 	
