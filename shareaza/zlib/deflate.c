2e7723bb017ee5b852488c1dd831e27ede2d0ca1
spooky23 <spooky23@3231ed4b-2112-0410-84e0-8c3944a40730>
2004-10-08 13:44:56 +0000

    Updated to Zlib Version 1.2.2
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@934 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/zlib/deflate.c
+++ shareaza/zlib/deflate.c
@@ -1,5 +1,5 @@
 /* deflate.c -- compress data using the deflation algorithm
- * Copyright (C) 1995-2003 Jean-loup Gailly.
+ * Copyright (C) 1995-2004 Jean-loup Gailly.
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
@@ -47,12 +47,12 @@
  *
  */
 
-/* @(#) $Id: deflate.c,v 1.1 2004/10/08 12:59:32 spooky23 Exp $ */
+/* @(#) $Id: deflate.c,v 1.2 2004/10/08 13:44:56 spooky23 Exp $ */
 
 #include "deflate.h"
 
 const char deflate_copyright[] =
-   " deflate 1.2.1 Copyright 1995-2003 Jean-loup Gailly ";
+   " deflate 1.2.2 Copyright 1995-2004 Jean-loup Gailly ";
 /*
   If you use the zlib library in a product, an acknowledgment is welcome
   in the documentation of your product. If for some reason you cannot
--- shareaza/zlib/deflate.h
+++ shareaza/zlib/deflate.h
@@ -8,7 +8,7 @@
    subject to change. Applications should only use zlib.h.
  */
 
-/* @(#) $Id: deflate.h,v 1.1 2004/10/08 12:59:32 spooky23 Exp $ */
+/* @(#) $Id: deflate.h,v 1.2 2004/10/08 13:44:56 spooky23 Exp $ */
 
 #ifndef DEFLATE_H
 #define DEFLATE_H
@@ -95,7 +95,6 @@ typedef struct internal_state {
     Bytef *pending_out;  /* next pending byte to output to the stream */
     int   pending;       /* nb of bytes in the pending buffer */
     int   wrap;          /* bit 0 true for zlib, bit 1 true for gzip */
-    Byte  data_type;     /* UNKNOWN, BINARY or ASCII */
     Byte  method;        /* STORED (for zip only) or DEFLATED */
     int   last_flush;    /* value of flush param for previous deflate call */
 
