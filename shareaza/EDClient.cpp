3076fd924bcb12187043e58cf5dc868420c0f85f
mogthecat <mogthecat@3231ed4b-2112-0410-84e0-8c3944a40730>
2005-03-11 18:20:31 +0000

    Lock optimisation
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@1637 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/EDClient.cpp
+++ shareaza/EDClient.cpp
@@ -1120,37 +1120,36 @@ BOOL CEDClient::OnFileRequest(CEDPacket* pPacket)
 	pReply->Write( &m_pUpMD4, sizeof(MD4) );
 	m_bUpMD4 = TRUE;
 	
+	CSingleLock oLock( &Library.m_pSection,TRUE );
+	CLibraryFile* pFile = LibraryMaps.LookupFileByED2K( &m_pUpMD4, TRUE, TRUE );
+	if ( ( pFile ) && ( UploadQueues.CanUpload( PROTOCOL_ED2K, pFile, TRUE ) ) )
 	{
-		CSingleLock oLock( &Library.m_pSection,TRUE );
-		CLibraryFile* pFile = LibraryMaps.LookupFileByED2K( &m_pUpMD4, TRUE, TRUE );
-		if ( ( pFile ) && ( UploadQueues.CanUpload( PROTOCOL_ED2K, pFile, TRUE ) ) )
-		{
-			// Create the reply packet
-			pReply->WriteEDString( pFile->m_sName, m_bEmUnicode );
+		// Create the reply packet
+		pReply->WriteEDString( pFile->m_sName, m_bEmUnicode );
 
-			// If we have not sent comments yet, and this client supports comments
-			if ( ( ! m_bCommentSent ) && ( m_bEmComments > 0 ) )
-			{ 
-				// If there's comments in the library
-				if ( ( pFile->m_nRating > 0 ) || ( pFile->m_sComments.GetLength() ) )
-				{
-					// Create the comments packet
-					pComment = CEDPacket::New( ED2K_C2C_FILEDESC );
-					pComment->WriteByte( (BYTE)min( pFile->m_nRating, 5 ) );
-					pComment->WriteEDString( pFile->m_sComments.Left(ED2K_COMMENT_MAX), m_bEmUnicode );
-					m_bCommentSent = TRUE;
-				}
+		// If we have not sent comments yet, and this client supports comments
+		if ( ( ! m_bCommentSent ) && ( m_bEmComments > 0 ) )
+		{ 
+			// If there's comments in the library
+			if ( ( pFile->m_nRating > 0 ) || ( pFile->m_sComments.GetLength() ) )
+			{
+				// Create the comments packet
+				pComment = CEDPacket::New( ED2K_C2C_FILEDESC );
+				pComment->WriteByte( (BYTE)min( pFile->m_nRating, 5 ) );
+				pComment->WriteEDString( pFile->m_sComments.Left(ED2K_COMMENT_MAX), m_bEmUnicode );
+				m_bCommentSent = TRUE;
 			}
-			oLock.Unlock();
-
-			// Send reply
-			Send( pReply );
-			// Send comments / rating
-			if ( pComment ) Send( pComment );
-
-			return TRUE;
 		}
+		oLock.Unlock();
+
+		// Send reply
+		Send( pReply );
+		// Send comments / rating
+		if ( pComment ) Send( pComment );
+
+		return TRUE;
 	}
+	oLock.Unlock();
 
 	if ( CDownload* pDownload = Downloads.FindByED2K( &m_pUpMD4, TRUE ) )
 	{
