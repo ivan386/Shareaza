ea337802a63d5f9077ea597aad09a389aee43643
rolandas <rolandas@3231ed4b-2112-0410-84e0-8c3944a40730>
2006-02-26 22:55:28 +0000

    Query search and hits set to UTF-8 by default
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@3203 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/Settings.cpp
+++ shareaza/Settings.cpp
@@ -252,8 +252,8 @@ void CSettings::Setup()
 	Add( _T("Gnutella1.PongCache"), &Gnutella1.PongCache, 10000 );
 	Add( _T("Gnutella1.PongCount"), &Gnutella1.PongCount, 10 );
 	Add( _T("Gnutella1.QueueLimter"), &Gnutella1.HitQueueLimit, 50 );
-	Add( _T("Gnutella1.QuerySearchUTF8"), &Gnutella1.QuerySearchUTF8, FALSE );
-	Add( _T("Gnutella1.QueryHitUTF8"), &Gnutella1.QueryHitUTF8, FALSE );
+	Add( _T("Gnutella1.QuerySearchUTF8"), &Gnutella1.QuerySearchUTF8, TRUE );
+	Add( _T("Gnutella1.QueryHitUTF8"), &Gnutella1.QueryHitUTF8, TRUE );
 
 	Add( _T("Gnutella2.ClientMode"), &Gnutella2.ClientMode, MODE_AUTO );
 	Add( _T("Gnutella2.HubVerified"), &Gnutella2.HubVerified, FALSE );
