c1d02dec6e6f0b31cd600267f2fa0cf41bc985f9
Rolandas <Rolandas@3231ed4b-2112-0410-84e0-8c3944a40730>
2006-11-06 15:38:03 +0000

    Weird resource editor bug fixed(?)
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@3943 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/resource.h
+++ shareaza/resource.h
@@ -1714,7 +1714,7 @@
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        278
 #define _APS_NEXT_COMMAND_VALUE         33136
-#define _APS_NEXT_CONTROL_VALUE         1438
+#define _APS_NEXT_CONTROL_VALUE         1439
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
