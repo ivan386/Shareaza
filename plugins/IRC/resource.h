d856741ec474e5fe9ddcac348c056aed5055d354
Rolandas <Rolandas@3231ed4b-2112-0410-84e0-8c3944a40730>
2007-01-25 23:32:15 +0000

    Committed a working copy (uncompilable)
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@4611 3231ed4b-2112-0410-84e0-8c3944a40730

--- plugins/IRC/resource.h
+++ plugins/IRC/resource.h
@@ -12,7 +12,6 @@
 #define IDI_IRCHEADER32                 205
 #define IDI_IRCHEADER                   206
 #define IDR_IRCFRAME                    207
-#define IDB_BITMAP1                     208
 #define IDB_IRCICONS                    208
 
 // Next default values for new objects
@@ -22,6 +21,6 @@
 #define _APS_NEXT_RESOURCE_VALUE        209
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         201
-#define _APS_NEXT_SYMED_VALUE           105
+#define _APS_NEXT_SYMED_VALUE           106
 #endif
 #endif
