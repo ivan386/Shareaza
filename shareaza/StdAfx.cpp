7a8055f26bacac3d65635a392bc4ac160b160fc6
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2007-04-21 08:54:12 +0000

    Internal compiler error workaround
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@5209 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/StdAfx.cpp
+++ shareaza/StdAfx.cpp
@@ -20,5 +20,72 @@
 //
 
 #include "StdAfx.h"
+#include "RegExp\regexpr2.h"
 
 const CLowerCaseTable ToLower;
+
+CLowerCaseTable::CLowerCaseTable()
+{
+	for ( size_t i = 0; i < 65536; ++i ) cTable[ i ] = TCHAR( i );
+	cTable[ 65536 ] = 0;
+	CharLower( &cTable[ 1 ] );
+	// turkish capital I with dot is converted to "i"
+	cTable[ 304 ] = 105;
+	// convert fullwidth latin characters to halfwidth
+	for ( size_t i = 65281 ; i < 65313 ; ++i ) cTable[ i ] = TCHAR( i - 65248 );
+	for ( size_t i = 65313 ; i < 65339 ; ++i ) cTable[ i ] = TCHAR( i - 65216 );
+	for ( size_t i = 65339 ; i < 65375 ; ++i ) cTable[ i ] = TCHAR( i - 65248 );
+	// convert circled katakana to ordinary katakana
+	for ( size_t i = 13008 ; i < 13028 ; ++i ) cTable[ i ] = TCHAR( 2 * i - 13566 );
+	for ( size_t i = 13028 ; i < 13033 ; ++i ) cTable[ i ] = TCHAR( i - 538 );
+	for ( size_t i = 13033 ; i < 13038 ; ++i ) cTable[ i ] = TCHAR( 3 * i - 26604 );
+	for ( size_t i = 13038 ; i < 13043 ; ++i ) cTable[ i ] = TCHAR( i - 528 );
+	for ( size_t i = 13043 ; i < 13046 ; ++i ) cTable[ i ] = TCHAR( 2 * i - 13571 );
+	for ( size_t i = 13046 ; i < 13051 ; ++i ) cTable[ i ] = TCHAR( i - 525 );
+	cTable[ 13051 ] = TCHAR( 12527 );
+	for ( size_t i = 13052 ; i < 13055 ; ++i ) cTable[ i ] = TCHAR( i - 524 );
+	// map Katakana middle dot to space, since no API identifies it as a punctuation
+	cTable[ 12539 ] = cTable[ 65381 ] = L' ';
+	// map CJK Fullwidth space to halfwidth space
+	cTable[ 12288 ] = L' ';
+	// convert japanese halfwidth sound marks to fullwidth
+	// all forms should be mapped; we need NFKD here
+	cTable[ 65392 ] = TCHAR( 12540 );
+	cTable[ 65438 ] = TCHAR( 12441 );
+	cTable[ 65439 ] = TCHAR( 12442 );
+}
+
+CString& CLowerCaseTable::operator()(CString& strSource) const
+{
+	const int nLength = strSource.GetLength();
+	const LPTSTR str = strSource.GetBuffer() + nLength;
+
+	bool bSigma = false;
+	for ( int i = -nLength; i; ++i ) 
+	{
+		if ( str[ i ] == 0x3a3 )
+		{
+			bSigma = true;
+			break;
+		}
+	}
+
+	if ( bSigma )
+	{
+		// Lowercase greek final sigmas first (word endings)
+		const regex::rpattern regExpPattern( _T("(\\w+)\x3a3(\\W|$)"), _T("$1\x3c2$2"), 
+		regex::GLOBAL|regex::MULTILINE|regex::NOBACKREFS, regex::MODE_SAFE );
+		regex::subst_results results;
+		std::wstring strTemp( strSource, nLength );
+		regExpPattern.substitute( strTemp, results );
+		strSource.SetString( strTemp.c_str(), nLength );
+	}
+
+	// Lowercase now everything. Not final sigmas are taken from the table
+	for ( int i = -nLength; i; ++i )
+		str[ i ] = ( *this )( str[ i ] );
+
+	strSource.ReleaseBuffer( nLength );
+
+	return strSource;
+}
