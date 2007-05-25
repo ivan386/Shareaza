90f1d522ad329b799a457c516f079fb7312ce74b
mikewalsh <mikewalsh@3231ed4b-2112-0410-84e0-8c3944a40730>
2007-05-25 13:33:36 +0000

    Set leaf to ultrapeer default to 3
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@5451 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/Settings.cpp
+++ shareaza/Settings.cpp
@@ -241,7 +241,7 @@ void CSettings::Setup()
 
 	Add( _T("Gnutella1.ClientMode"), &Gnutella1.ClientMode, MODE_LEAF );
 	Add( _T("Gnutella1.EnableAlways"), &Gnutella1.EnableAlways, FALSE );
-	Add( _T("Gnutella1.NumHubs"), &Gnutella1.NumHubs, 5 );
+	Add( _T("Gnutella1.NumHubs"), &Gnutella1.NumHubs, 3 );
 	Add( _T("Gnutella1.NumLeafs"), &Gnutella1.NumLeafs, 0 );
 	Add( _T("Gnutella1.NumPeers"), &Gnutella1.NumPeers, 32 ); // For X-Degree
 	Add( _T("Gnutella1.PacketBufferSize"), &Gnutella1.PacketBufferSize, 64 );
