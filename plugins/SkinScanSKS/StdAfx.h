acbfc3d954dea50fc1788174aba34973b1ac6d75
wildcard_25 <wildcard_25@3231ed4b-2112-0410-84e0-8c3944a40730>
2007-12-01 11:10:25 +0000

    Suppress ATL compile warnings.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@6327 3231ed4b-2112-0410-84e0-8c3944a40730

--- plugins/SkinScanSKS/StdAfx.h
+++ plugins/SkinScanSKS/StdAfx.h
@@ -36,6 +36,8 @@
 #define _ATL_CSTRING_NO_CRT
 #define _ATL_ALL_WARNINGS
 
+#pragma warning( push, 0 )
+
 #include <atlbase.h>
 #include <atlcom.h>
 #include <atlstr.h>
@@ -43,3 +45,5 @@
 using namespace ATL;
 
 #include <unzip.h>
+
+#pragma warning( pop )
