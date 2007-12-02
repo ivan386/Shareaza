77c8f701df85fb64ccb1ddec495c86ffd3f6223f
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2007-12-02 06:49:41 +0000

    Updated _APS_NEXT_COMMAND_VALUE
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@6357 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/resource.h
+++ shareaza/resource.h
@@ -1753,7 +1753,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        287
-#define _APS_NEXT_COMMAND_VALUE         33138
+#define _APS_NEXT_COMMAND_VALUE         33203
 #define _APS_NEXT_CONTROL_VALUE         1535
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
