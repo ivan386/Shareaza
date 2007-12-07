5b27a810b8891168f1836f75a63241c4ffd08137
ale5000 <ale5000@3231ed4b-2112-0410-84e0-8c3944a40730>
2007-12-07 21:45:08 +0000

    Give warning message if Inno Setup isn't installed.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@6403 3231ed4b-2112-0410-84e0-8c3944a40730

--- vc7_1/MakeInstaller.bat
+++ vc7_1/MakeInstaller.bat
@@ -1,3 +1,7 @@
-set OLDDIR=%CD%
-cd "..\setup\scripts"
-"%ProgramFiles%\Inno Setup 5\ISCC.exe" main.iss /d%1 /dPlatformName=%2 /o"%OLDDIR%\Installer\"
+if exist "%ProgramFiles%\Inno Setup 5\ISCC.exe" (
+	set OLDDIR=%CD%
+	cd "..\setup\scripts"
+	"%ProgramFiles%\Inno Setup 5\ISCC.exe" main.iss /d%1 /dPlatformName=%2 /o"%OLDDIR%\Installer\"
+) else (
+	echo You must have Inno Setup 5 installed before you can create the installer.
+)
