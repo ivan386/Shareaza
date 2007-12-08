5ce05185755bd40e874e323c0819fe5586be1d12
wildcard_25 <wildcard_25@3231ed4b-2112-0410-84e0-8c3944a40730>
2007-12-08 12:34:07 +0000

    Save Installer to the right directory if sources are on a different drive to Inno Setup.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@6406 3231ed4b-2112-0410-84e0-8c3944a40730

--- vc8_0/MakeInstaller.bat
+++ vc8_0/MakeInstaller.bat
@@ -1,5 +1,5 @@
+set OLDDIR=%CD%
 if exist "%ProgramFiles%\Inno Setup 5\ISCC.exe" (
-	set OLDDIR=%CD%
 	cd "..\setup\scripts"
 	"%ProgramFiles%\Inno Setup 5\ISCC.exe" main.iss /d%1 /dPlatformName=%2 /o"%OLDDIR%\Installer\"
 ) else (
