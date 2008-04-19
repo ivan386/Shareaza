4620cd6b1ab1b10c3a2c6d361c0328ffc46df872
wildcard_25 <wildcard_25@3231ed4b-2112-0410-84e0-8c3944a40730>
2008-04-19 03:39:01 +0000

    Revert accidental regression in r6894.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@6895 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/StdAfx.h
+++ shareaza/StdAfx.h
@@ -488,8 +488,8 @@ extern const CLowerCaseTable ToLower;
 		if ( ( h ) == INVALID_HANDLE_VALUE ) \
 		{ \
 			DWORD err = GetLastError(); \
-			TRACE( _T("File error \"%ls\": %s (0x%08x)\n"), \
-				LPCTSTR((f)), LPCTSTR( GetErrorString( err ) ), err ); \
+			theApp.Message( MSG_DEBUG, _T("File error \"%s\": %s (0x%08x)"), \
+				LPCTSTR( f ), LPCTSTR( GetErrorString( err ) ), err ); \
 		} \
 	}
 #else
