fc7e10bef7b4d20aeea36f0d53cf05dfc18b9504
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2008-05-24 06:01:51 +0000

    Rolledback r6991 since it produces lost library files (need different fix to reduce CPU time for gigantic libraries)
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@6992 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/Library.cpp
+++ shareaza/Library.cpp
@@ -534,11 +534,10 @@ BOOL CLibrary::ThreadScan()
 	BOOL bForcedScan = ( m_nForcedUpdateCookie < m_nUpdateCookie ) &&
 		( m_nUpdateCookie > tNow - Settings.Library.WatchFoldersTimeout * 1000 );
 
-	if ( bPeriodicScan || bForcedScan )
+	bChanged = LibraryFolders.ThreadScan( &m_bThread, ( bPeriodicScan || bForcedScan ) );
+
+	if ( bPeriodicScan || bForcedScan || bChanged )
 	{
-
-		bChanged = LibraryFolders.ThreadScan( &m_bThread, bForcedScan );
-
 		m_nScanTime = GetTickCount();
 
 		if ( bForcedScan )
