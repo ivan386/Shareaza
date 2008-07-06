77ee53adf920053faa349211f8d9346d60cd1030
rolandas <rolandas@3231ed4b-2112-0410-84e0-8c3944a40730>
2008-07-06 12:52:38 +0000

    Added GPL licence comments
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@7110 3231ed4b-2112-0410-84e0-8c3944a40730

--- plugins/NETInterop/FictionBookReader/ShareazaBook.cs
+++ plugins/NETInterop/FictionBookReader/ShareazaBook.cs
@@ -1,3 +1,24 @@
+//
+// ShareazaBook.cs
+//
+// Copyright (c) Shareaza Development Team, 2008.
+// This file is part of SHAREAZA (shareaza.sourceforge.net)
+//
+// Shareaza is free software; you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2 of
+// the License, or (at your option) any later version.
+//
+// Shareaza is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Shareaza; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#region Using directives
 using System;
 using System.Collections;
 using System.Diagnostics;
@@ -6,6 +27,8 @@ using System.Text;
 using System.Threading;
 using System.Xml;
 using System.Xml.Serialization;
+
+#endregion
 
 namespace Schemas
 {
