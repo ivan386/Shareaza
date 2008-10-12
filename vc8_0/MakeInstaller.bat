dad218367c8b196733a76a6b343e83e4aac72a57
wildcard_25 <wildcard_25@3231ed4b-2112-0410-84e0-8c3944a40730>
2008-10-12 01:50:43 +0000

    Change warning message to advise installation of QuickStart Pack instead of vanilla Inno Setup 5.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@7453 3231ed4b-2112-0410-84e0-8c3944a40730

--- vc8_0/MakeInstaller.bat
+++ vc8_0/MakeInstaller.bat
@@ -3,5 +3,5 @@ if exist "%ProgramFiles%\Inno Setup 5\ISCC.exe" (
 	cd "..\setup"
 	"%ProgramFiles%\Inno Setup 5\ISCC.exe" scripts\main.iss /dConfigurationName=%1 /dPlatformName=%2 /o"%OLDDIR%\Installer\"
 ) else (
-	echo You must have Inno Setup 5 installed before you can create the installer.
+	echo You must have the 'Inno Setup 5 QuickStart Pack' installed before you can create the installer.
 )
