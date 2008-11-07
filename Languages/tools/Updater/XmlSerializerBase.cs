a4d884bcead2aaef64684a0531e479a797441de4
rolandas <rolandas@3231ed4b-2112-0410-84e0-8c3944a40730>
2008-11-07 19:39:35 +0000

    Removed XML serializer caching (not portable to Mono); thanks Brov for the report and tests under Mono framework in Linux
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@7570 3231ed4b-2112-0410-84e0-8c3944a40730

--- Languages/tools/Updater/XmlSerializerBase.cs
+++ Languages/tools/Updater/XmlSerializerBase.cs
@@ -5,23 +5,17 @@ using System.Security.Permissions;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
-using Mvp.Xml.Common.Serialization;
 
 #endregion
 
 namespace ShareazaDialogUpdater
 {
-	public class XmlCache
-	{
-		protected static XmlSerializerCache _cache = new XmlSerializerCache();
-	}
-
-	public class XmlSerializerBase<T> : XmlCache where T : class
+	public class XmlSerializerBase<T> where T : class
 	{
 		static XmlSerializer serializer;
 		
 		static XmlSerializerBase() {
-			serializer = _cache.GetSerializer(typeof(T), new Type[] { typeof(XmlElement) });
+			serializer = new XmlSerializer(typeof(T), new Type[] { typeof(XmlElement) });
 			serializer.UnknownNode += new XmlNodeEventHandler(OnUnknownNode);
 			serializer.UnknownAttribute += new XmlAttributeEventHandler(OnUnknownAttribute);
 		}
@@ -83,7 +77,6 @@ namespace ShareazaDialogUpdater
 
 		[PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
 		public static void Write(T file, string filePath) {
-			var serializer = _cache.GetSerializer(file.GetType(), new Type[] { typeof(XmlElement) });
 			var xns = new XmlSerializerNamespaces();
 			xns.Add(string.Empty, string.Empty);
 
@@ -110,7 +103,6 @@ namespace ShareazaDialogUpdater
 		}
 
 		public static bool Write(T objectClass, out MemoryStream stream) {
-			var serializer = _cache.GetSerializer(objectClass.GetType(), new Type[] { typeof(XmlElement) });
 			var xns = new XmlSerializerNamespaces();
 			xns.Add(string.Empty, string.Empty);
 
