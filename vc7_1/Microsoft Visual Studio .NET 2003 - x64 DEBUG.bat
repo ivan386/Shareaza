e4b4921e81d9a01f8b31abff0ffb0c58cd6a7b5b
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2009-03-23 19:17:24 +0000

    Removed outdated VS 2003 support.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@7786 3231ed4b-2112-0410-84e0-8c3944a40730

--- vc7_1/Microsoft Visual Studio .NET 2003 - x64 DEBUG.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set OLDDIR=%CD%
-CD /D "%MSSdk%"
-call setenv.cmd /X64 /DEBUG
-set include=%include%;C:\Boost\include\boost-1_33_1
-CD /D "%VS71COMNTOOLS%..\IDE\"
-start devenv.exe "%OLDDIR%\Shareaza.sln" /useenv
