ee068a43ba983d97a7a5f9e432868fe3641a245b
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2009-06-15 16:23:27 +0000

    Removed orphan icons and manifest
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@8030 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/resource.h
+++ shareaza/resource.h
@@ -107,8 +107,6 @@
 #define IDR_LARGE_LOGO                  196
 #define IDD_SETTINGS_WEB                198
 #define IDD_PROFILE_IDENTITY            199
-#define IDI_HOST_OUTGOING               200
-#define IDI_HOST_INCOMING               201
 #define IDD_PROFILE_FAVOURITES          201
 #define IDI_PROPERTIES                  202
 #define IDD_PROFILE_PROFILE             202
