69dff421767305acae7ecc9982abe1da2524b4eb
rolandas <rolandas@3231ed4b-2112-0410-84e0-8c3944a40730>
2009-06-16 18:04:56 +0000

    Updated Shareaza Interop library and the FictionBook plugin
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@8035 3231ed4b-2112-0410-84e0-8c3944a40730

--- plugins/NETInterop/FictionBookReader/tlbimport.bat
+++ plugins/NETInterop/FictionBookReader/tlbimport.bat
@@ -1,4 +1,5 @@
 cd ..
 cd ..
 cd ..
-"%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\tlbimp.exe" "..\..\shareaza\win32\%1\Shareaza.tlb" /out:Shareaza.Interop.dll /namespace:Shareaza /asmversion:1.0.0.0 /keyfile:Shareaza.snk /nologo /primary /sysarray /machine:x86 /transform:DispRet
+echo %CD%
+"%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\tlbimp.exe" "..\..\shareaza\win32\%1\Shareaza.tlb" /out:Shareaza.Interop.dll /namespace:Shareaza /asmversion:1.1.0.0 /keyfile:Shareaza.snk /nologo /primary /sysarray /machine:x86 /transform:DispRet
