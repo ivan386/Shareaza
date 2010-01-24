ddcd7ce4560750bcd4f24e102bf96b09b052d2b9
wildcard_25 <wildcard_25@3231ed4b-2112-0410-84e0-8c3944a40730>
2010-01-24 02:02:20 +0000

    Fix off-by-one error for piece selection with BT and ED2K. Make selection slightly more intelligent.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@8444 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/FileFragments/Compatibility.hpp
+++ shareaza/FileFragments/Compatibility.hpp
@@ -1,7 +1,7 @@
 //
 // Fragments/Compatibility.hpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2007.
+// Copyright (c) Shareaza Development Team, 2002-2010.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -19,11 +19,8 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 
-#ifndef FRAGMENTS_COMPATIBILITY_HPP_INCLUDED
-#define FRAGMENTS_COMPATIBILITY_HPP_INCLUDED
+#pragma once
 
-#pragma warning( push )
-#pragma warning( disable : 4996 )
 
 namespace Fragments
 {
@@ -39,50 +36,124 @@ typename list_type::range_type selectBlock(const list_type& src,
 	typedef typename list_type::range_size_type range_size_type;
 	typedef typename list_type::const_iterator const_iterator;
 
-	if ( src.empty() ) return range_type( 0, 0 );
+	if ( src.empty() )
+		return range_type( 0, 0 );
 
 	std::deque< range_size_type > blocks;
+	range_size_type range_begin = 0;
+	range_size_type range_size = 0;
+	range_size_type range_block = 0;
+	range_size_type range_total = 0;
+	range_size_type best_range_begin = 0;
+	range_size_type best_range_size = 0;
+	range_size_type best_range_total = 0;
 
-	for ( const_iterator select = src.begin(); select != src.end(); ++select )
+	const_iterator pItr = src.begin();
+	const const_iterator pEnd = src.end();
+
+	if ( pItr->begin() < Settings.Downloads.ChunkStrap )
+		return range_type( pItr->begin(), min( pItr->end(), Settings.Downloads.ChunkStrap ) );
+
+	for ( ; pItr != pEnd ; ++pItr )
 	{
-		range_size_type block_begin = select->begin() / block_size;
-		if ( select->begin() % block_size )
+		range_size_type block_begin = pItr->begin() / block_size;
+		range_size_type block_end = ( pItr->end() - 1 ) / block_size;
+		range_size_type part_begin = pItr->begin();
+		range_size_type part_size = 0;
+
+		// The start of a block is complete, but part is missing
+		if ( part_begin % block_size
+			&& ( !available || available[ block_begin ] ) )
 		{
-			// the start of a block is complete, but part is missing
-			
-			++block_begin;
-			if ( !available || available[ block_begin ] )
+			part_size = min( pItr->end(), block_size * ( block_begin + 1 ) );
+			part_size -= part_begin;
+			if ( block_begin == range_block )
 			{
-				return range_type( select->begin(),
-					min( select->end(), block_size * block_begin ) );
+				if ( part_size < range_size || !range_size )
+				{
+					range_begin = part_begin;
+					range_size = part_size;
+				}
+				range_total += part_size;
+			}
+			else
+			{
+				if ( range_total < best_range_total
+					|| ( range_total && !best_range_total ) )
+				{
+					best_range_begin = range_begin;
+					best_range_size = range_size;
+					best_range_total = range_total;
+				}
+				range_begin = part_begin;
+				range_size = range_total = part_size;
+				range_block = block_begin;
 			}
 		}
-		range_size_type block_end = ( select->end() - 1 ) / block_size + 1;
-		if ( block_begin != block_end && select->end() % block_size )
+
+		// the end of a block is complete, but part is missing
+		if ( ( !part_size || block_begin != block_end )
+			&& pItr->end() % block_size
+			&& ( !available || available[ block_end ] ) )
 		{
-			// the end of a block is complete, but part is missing
-			
-			--block_end;
-			if ( !available || available[ block_end ] )
+			part_begin = block_end * block_size;
+			part_size = pItr->end() - part_begin;
+			if ( block_end == range_block )
 			{
-				return range_type( block_end * block_size, select->end() );
+				if ( part_size < range_size || !range_size )
+				{
+					range_begin = part_begin;
+					range_size = part_size;
+				}
+				range_total += part_size;
+			}
+			else
+			{
+				if ( range_total < best_range_total
+					|| ( range_total && !best_range_total ) )
+				{
+					best_range_begin = range_begin;
+					best_range_size = range_size;
+					best_range_total = range_total;
+				}
+				range_begin = part_begin;
+				range_size = range_total = part_size;
+				range_block = block_end;
 			}
 		}
+
 		// this fragment contains one or more aligned empty blocks
-		for ( ; block_begin != block_end; ++block_begin )
+		if ( !range_total )
 		{
-			if ( !available || available[ block_begin ] )
+			for ( ; block_begin <= block_end; ++block_begin )
 			{
-				blocks.push_back( block_begin );
+				if ( !available || available[ block_begin ] )
+					blocks.push_back( block_begin );
 			}
 		}
 	}
 
-	if ( blocks.empty() ) return range_type( 0, 0 );
+	if ( range_total < best_range_total
+		|| ( range_total && !best_range_total ) )
+	{
+		best_range_begin = range_begin;
+		best_range_size = range_size;
+		best_range_total = range_total;
+	}
 
-	range_size_type block = blocks[ GetRandomNum( 0ui64, (uint64)blocks.size() - 1 ) ] * block_size;
+	if ( !best_range_total )
+	{
+		if ( blocks.empty() )
+			return range_type( 0, 0 );
+		else
+		{
+			range_begin = blocks[ GetRandomNum( 0ui64, (uint64)blocks.size() - 1 ) ];
+			range_begin *= block_size;
+			return range_type( range_begin, range_begin + block_size );
+		}
+	}
 
-	return range_type( block, min( block + block_size, src.limit() ) );
+	return range_type( best_range_begin, best_range_begin + best_range_size );
 }
 
 inline void SerializeOut(CArchive& ar, const Ranges::Range< uint64 >& out)
@@ -98,20 +169,24 @@ inline Ranges::Range< uint64 > SerializeIn(CArchive& ar, int version)
 		{
 			uint64 begin, length;
 			ar >> begin >> length;
-			if ( begin + length < begin ) AfxThrowArchiveException( CArchiveException::generic );
+			if ( begin + length < begin )
+				AfxThrowArchiveException( CArchiveException::genericException );
+
 			return Ranges::Range< uint64 >( begin, begin + length );
 		}
 		else
 		{
 			uint32 begin, length;
 			ar >> begin >> length;
-			if ( begin + length < begin ) AfxThrowArchiveException( CArchiveException::generic );
+			if ( begin + length < begin )
+				AfxThrowArchiveException( CArchiveException::genericException );
+
 			return Ranges::Range< uint64 >( begin, begin + length );
 		}
 	}
 	catch ( Exception& )
 	{
-		AfxThrowArchiveException( CArchiveException::generic );
+		AfxThrowArchiveException( CArchiveException::genericException );
 	}
 }
 
@@ -148,15 +223,18 @@ inline void SerializeIn1(CArchive& ar,
 			for ( ; nFragments--; )
 			{
 				const Ranges::Range< uint64 >& fragment = SerializeIn( ar, version );
-				if ( fragment.end() > nTotal ) AfxThrowArchiveException( CArchiveException::generic );
+				if ( fragment.end() > nTotal )
+					AfxThrowArchiveException( CArchiveException::genericException );
+
 				in.insert( in.end(), fragment );
 			}
 			// Sanity check
-			if ( in.length_sum() != nRemaining ) AfxThrowArchiveException( CArchiveException::generic );
+			if ( in.length_sum() != nRemaining )
+				AfxThrowArchiveException( CArchiveException::genericException );
 		}
 		catch ( Exception& )
 		{
-			AfxThrowArchiveException( CArchiveException::generic );
+			AfxThrowArchiveException( CArchiveException::genericException );
 		}
 	}
 	else
@@ -173,15 +251,18 @@ inline void SerializeIn1(CArchive& ar,
 			for ( ; nFragments--; )
 			{
 				const Ranges::Range< uint64 >& fragment = SerializeIn( ar, version );
-				if ( fragment.end() > nTotal ) AfxThrowArchiveException( CArchiveException::generic );
+				if ( fragment.end() > nTotal )
+					AfxThrowArchiveException( CArchiveException::genericException );
+
 				in.insert( in.end(), fragment );
 			}
 			// Sanity check
-			if ( in.length_sum() != nRemaining ) AfxThrowArchiveException( CArchiveException::generic );
+			if ( in.length_sum() != nRemaining )
+				AfxThrowArchiveException( CArchiveException::genericException );
 		}
 		catch ( Exception& )
 		{
-			AfxThrowArchiveException( CArchiveException::generic );
+			AfxThrowArchiveException( CArchiveException::genericException );
 		}
 	}
 }
@@ -209,7 +290,8 @@ inline void SerializeIn2(CArchive& ar,
 			for ( DWORD_PTR count = ar.ReadCount(); count--; )
 			{
 				const Ranges::Range< uint64 >& fragment = SerializeIn( ar, version );
-				if ( fragment.end() > in.limit() ) AfxThrowArchiveException( CArchiveException::generic );
+				if ( fragment.end() > in.limit() )
+					AfxThrowArchiveException( CArchiveException::genericException );
 				in.insert( in.end(), fragment );
 			}
 		}
@@ -218,19 +300,16 @@ inline void SerializeIn2(CArchive& ar,
 			while ( ar.ReadCount() )
 			{
 				const Ranges::Range< uint64 >& fragment = SerializeIn( ar, version );
-				if ( fragment.end() > in.limit() ) AfxThrowArchiveException( CArchiveException::generic );
+				if ( fragment.end() > in.limit() )
+					AfxThrowArchiveException( CArchiveException::genericException );
 				in.insert( in.end(), fragment );
 			}
 		}
 	}
 	catch ( Exception& )
 	{
-		AfxThrowArchiveException( CArchiveException::generic );
+		AfxThrowArchiveException( CArchiveException::genericException );
 	}
 }
 
 } // namespace Fragments
-
-#pragma warning( pop )
-
-#endif // #ifndef FRAGMENTS_COMPATIBILITY_HPP_INCLUDED
