4b710847fcfa876ce4cbf19ba5619be24beac56c
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2010-02-18 14:54:35 +0000

    Fixed hidden "open download" button.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@8478 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/WndDownloads.cpp
+++ shareaza/WndDownloads.cpp
@@ -976,7 +976,7 @@ void CDownloadsWnd::OnUpdateDownloadsLaunchComplete(CCmdUI* pCmdUI)
 {
 	Prepare();
 	if ( CCoolBarItem* pItem = CCoolBarItem::FromCmdUI( pCmdUI ) )
-		pItem->Show( m_bSelCompleted );
+		pItem->Show( m_bSelCompleted || ! m_bSelStartedAndNotMoving );
 	pCmdUI->Enable( m_bSelCompleted );
 }
 
