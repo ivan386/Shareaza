b67a4fe867a3505cd7ceca2e6be78b9da7e8fe5c
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2010-07-10 10:43:42 +0000

    Added VS2010 project files, removed VS2003. Not fully compilable yet.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@8677 3231ed4b-2112-0410-84e0-8c3944a40730

--- vc8_0/MakeInstaller.bat
+++
@@ -1,7 +0,0 @@
-set OLDDIR=%CD%
-if exist "%ProgramFiles%\Inno Setup 5\ISCC.exe" (
-	cd "..\setup"
-	"%ProgramFiles%\Inno Setup 5\ISCC.exe" scripts\main.iss /dConfigurationName=%1 /dPlatformName=%2 /o"%OLDDIR%\Installer\"
-) else (
-	echo You must have the 'Inno Setup 5 QuickStart Pack' installed before you can create the installer.
-)
