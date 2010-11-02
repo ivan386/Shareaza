1b391210455f32759392e9dfe534f989f56e07f1
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2010-11-02 15:15:39 +0000

    Updated applications manifests (in VS2008 style).
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@8838 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/StdAfx.h
+++ shareaza/StdAfx.h
@@ -688,3 +688,15 @@ INT_PTR MsgBox(UINT nIDPrompt, UINT nType = MB_OK, UINT nIDHelp = 0, DWORD* pnDe
 	((hr)==CO_E_OBJNOTCONNECTED)||\
 	((hr)==RPC_E_SERVERFAULT)||\
 	((hr)==RPC_E_INVALID_OBJECT))
+
+#ifdef _UNICODE
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_IA64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+#endif
