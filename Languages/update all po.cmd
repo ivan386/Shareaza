9b7a4b3d3686964e4e7e96c346a81430e4b61add
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2010-11-29 19:47:33 +0000

    Language update script: No fuzzy matching option.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@8873 3231ed4b-2112-0410-84e0-8c3944a40730

--- Languages/update all po.cmd
+++ Languages/update all po.cmd
@@ -1,2 +1,2 @@
 @echo off
-for %%i in (*.po) do msgmerge.exe --no-escape --quiet --no-wrap --update --backup=off "%%i" default-en.pot
+for %%i in (*.po) do msgmerge.exe --no-fuzzy-matching --no-escape --quiet --no-wrap --update --backup=off "%%i" default-en.pot
