ce68eacd0b04849ec422b7e79b1aec34bdf1c487
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2012-02-21 14:37:38 +0000

    Updated translations.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@9090 3231ed4b-2112-0410-84e0-8c3944a40730

--- Languages/update all po.cmd
+++ Languages/update all po.cmd
@@ -1,2 +1,2 @@
 @echo off
-for %%i in (*.po) do msgmerge.exe --no-escape --quiet --no-wrap --update --backup=off "%%i" default-en.pot
+for %%i in (*.po) do msgmerge.exe --no-escape --quiet --no-wrap --update --backup=off --no-fuzzy-matching "%%i" default-en.pot
