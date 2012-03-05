ivan386/Shareaza

commit 61eb2f6fd7850f6676849b445999c8df38450d93

Removed downloads extra checks, adjusted downloading file open time (more earlier). Fixed torrent file page of download properties dialog for single file torrents.
git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@9113 3231ed4b-2112-0410-84e0-8c3944a40730

shareaza/DownloadWithTorrent.cpp
@@ -410,10 +410,10 @@ BOOL CDownloadWithTorrent::SetTorrent(const CBTInfo* pTorrent)
 		HostCache.BitTorrent.Add( m_pTorrent.m_oNodes.GetNext( pos ) );
 	}
 
-	SetModified();
-
 	// Re-link
 	DownloadGroups.Link( static_cast< CDownload* >( this ) );
+
+	OpenFile();
 
 	return TRUE;
 }
@@ -426,20 +426,12 @@ bool CDownloadWithTorrent::RunTorrent(DWORD tNow)
 	if ( !Network.IsConnected() || !Settings.BitTorrent.EnableToday )
 		return true;
 
-	// Return if disk is full
-	if ( GetFileError() != ERROR_SUCCESS )
-		return false;
-
 	// Choke torrents every 10 seconds
 	if ( tNow > m_tTorrentChoke && tNow - m_tTorrentChoke >= 10000ul )
 		ChokeTorrent( tNow );
 
 	// Check if the torrent file exists and has been opened
-	if ( ! OpenFile() )
-		return false;
-
-	// Return if this download is waiting for a download task to finish
-	if ( IsTasking() )
+	if ( ! IsFileOpen() )
 		return false;
 
 	// Generate a peerid if there isn't one
@@ -452,12 +444,11 @@ bool CDownloadWithTorrent::RunTorrent(DWORD tNow)
 	DWORD nSourcesWanted = 0ul;
 
 	// Check if a tracker has already been locked onto
-	if ( !m_bTorrentStarted )
+	if ( ! m_bTorrentStarted )
 	{
 		// Check if download is active, isn't already waiting for a request
 		// reply and is allowed to try and contact this tracker
-		if ( !IsPaused() && ( IsTrying() || IsSeeding() )
-			&& !m_bTorrentRequested && tNow > m_tTorrentTracker )
+		if ( ! m_bTorrentRequested && ( tNow > m_tTorrentTracker ) )
 		{
 			// Get the # of sources that can be connected to
 			nSourcesCount = GetBTSourceCount( TRUE );

shareaza/Download.cpp
@@ -1,7 +1,7 @@
 //
 // Download.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2011.
+// Copyright (c) Shareaza Development Team, 2002-2012.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -102,7 +102,7 @@ void CDownload::Resume()
 	if ( IsCompleted() )
 		return;
 
-	if ( !IsPaused() )
+	if ( ! IsPaused() )
 	{
 		StartTrying();
 		return;
@@ -397,7 +397,7 @@ void CDownload::OnRun()
 
 	DWORD tNow = GetTickCount();
 
-	if ( !IsPaused() )
+	if ( ! IsPaused() )
 	{
 		if ( GetFileError() != ERROR_SUCCESS  )
 		{
@@ -410,7 +410,9 @@ void CDownload::OnRun()
 				OnDownloaded();
 		}
 		else if ( IsTrying() || IsSeeding() )
-		{	//This download is trying to download
+		{
+			// This download is trying to download
+			OpenFile();
 
 			//'Dead download' check- if download appears dead, give up and allow another to start.
 			if ( ! IsCompleted() && ( tNow - GetStartTimer() ) > ( 3 * 60 * 60 * 1000 )  )
@@ -473,29 +475,26 @@ void CDownload::OnRun()
 			} // if ( RunTorrent( tNow ) )
 
 			// Calculate the current downloading state
-			if( HasActiveTransfers() )
+			if ( HasActiveTransfers() )
 				m_bDownloading = true;
 		}
 		else if ( ! IsCompleted() && m_bVerify != TRI_TRUE )
 		{
+			// This is pending download
 			if ( Network.IsConnected() )
 			{
 				// We have extra regular downloads 'trying' so when a new slot
 				// is ready, a download has sources and is ready to go.
 				if ( Downloads.GetTryingCount() < ( Settings.Downloads.MaxFiles + Settings.Downloads.MaxFileSearches ) )
 				{
-					if ( IsTorrent() )
-					{
+					if ( ! IsTorrent() ||
 						// Torrents only try when 'ready to go'. (Reduce tracker load)
-						if ( Downloads.GetTryingCount( true ) < Settings.BitTorrent.DownloadTorrents )
-							Resume();
-					}
-					else
+						Downloads.GetTryingCount( true ) < Settings.BitTorrent.DownloadTorrents )
+					{
 						Resume();
+					}
 				}
 			}
-			else
-				ASSERT( !IsTrying() );
 		}
 	}
 

shareaza/DownloadWithSearch.cpp
@@ -1,7 +1,7 @@
 //
 // DownloadWithSearch.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2011.
+// Copyright (c) Shareaza Development Team, 2002-2012.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -105,7 +105,7 @@ void CDownloadWithSearch::RunSearch(DWORD tNow)
 		
 		m_tSearchCheck = tNow;
 		
-		if ( IsPaused() == FALSE && ( bFewSources || bDataStarve ) )
+		if ( bFewSources || bDataStarve )
 		{
 			StartAutomaticSearch();
 		}

shareaza/DownloadWithTiger.cpp
@@ -1,7 +1,7 @@
 //
 // DownloadWithTiger.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2011.
+// Copyright (c) Shareaza Development Team, 2002-2012.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -422,21 +422,15 @@ void CDownloadWithTiger::RunValidation()
 	if ( m_pTigerBlock == NULL && m_pHashsetBlock == NULL && m_pTorrentBlock == NULL )
 		return;
 
-	if ( ! OpenFile() )
+	if ( ! IsFileOpen() )
 		return;
 
-	if ( m_nVerifyHash > HASH_NULL && m_nVerifyBlock < 0xFFFFFFFF )
+	if ( ( m_nVerifyHash > HASH_NULL && m_nVerifyBlock < 0xFFFFFFFF ) ||
+		FindNewValidationBlock( HASH_TORRENT ) ||
+		FindNewValidationBlock( HASH_TIGERTREE ) ||
+		FindNewValidationBlock( HASH_ED2K ) )
 	{
 		ContinueValidation();
-	}
-	else
-	{
-		if ( FindNewValidationBlock( HASH_TORRENT ) ||
-			 FindNewValidationBlock( HASH_TIGERTREE ) ||
-			 FindNewValidationBlock( HASH_ED2K ) )
-		{
-			ContinueValidation();
-		}
 	}
 }
 
@@ -591,13 +585,8 @@ BOOL CDownloadWithTiger::FindNewValidationBlock(int nHash)
 
 void CDownloadWithTiger::ContinueValidation()
 {
-	CQuickLock oLock( m_pTigerSection );
-
 	ASSERT( m_nVerifyHash > HASH_NULL );
 	ASSERT( m_nVerifyBlock < 0xFFFFFFFF );
-
-	if ( ! OpenFile() )
-		return;
 
 	auto_array< BYTE > pChunk( new BYTE[ 256 * 1024ull ] );
 

shareaza/PageTorrentFiles.cpp
@@ -1,7 +1,7 @@
 //
 // PageTorrentGeneral.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2010.
+// Copyright (c) Shareaza Development Team, 2002-2012.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -77,8 +77,9 @@ BOOL CTorrentFilesPage::OnInitDialog()
 
 	CDownload* pDownload = ((CDownloadSheet*)GetParent())->GetDownload();
 	ASSERT( pDownload && pDownload->IsTorrent() );
-
-	bool bCompleted = pDownload->IsCompleted();
+	CComPtr< CFragmentedFile > pFragFile = pDownload->GetFile();
+	DWORD nCount = pFragFile ? pFragFile->GetCount() : 0;
+	bool bCompleted = pDownload->IsCompleted() || nCount < 2;
 
 	auto_ptr< CLibraryTipCtrl > pTip( new CLibraryTipCtrl );
 	pTip->Create( this, &Settings.Interface.TipDownloads );
@@ -106,22 +107,18 @@ BOOL CTorrentFilesPage::OnInitDialog()
 		END_COLUMN_MAP( m_wndFiles, 3 )
 	}
 
-	if ( CComPtr< CFragmentedFile > pFragFile = pDownload->GetFile() )
+	for ( DWORD i = 0; i < nCount; ++i )
 	{
-		for ( DWORD i = 0 ; i < pFragFile->GetCount() ; ++i )
-		{
-			LV_ITEM pItem = {};
-			pItem.mask		= LVIF_TEXT|LVIF_IMAGE|LVIF_PARAM;
-			pItem.iItem		= i;
-			pItem.lParam	= (LPARAM)pFragFile->GetAt( i );
-			pItem.iImage	= ShellIcons.Get( pFragFile->GetName( i ), 16 );
-			pItem.pszText	= (LPTSTR)(LPCTSTR)pFragFile->GetName( i );
-			pItem.iItem		= m_wndFiles.InsertItem( &pItem );
-			m_wndFiles.SetItemText( pItem.iItem, 1,
-				Settings.SmartVolume( pFragFile->GetLength( i ) ) );
-			if ( ! bCompleted )
-				m_wndFiles.SetColumnData( pItem.iItem, 3, pFragFile->GetPriority( i ) );
-		}
+		LV_ITEM pItem = {};
+		pItem.mask		= LVIF_TEXT|LVIF_IMAGE|LVIF_PARAM;
+		pItem.iItem		= i;
+		pItem.lParam	= (LPARAM)pFragFile->GetAt( i );
+		pItem.iImage	= ShellIcons.Get( pFragFile->GetName( i ), 16 );
+		pItem.pszText	= (LPTSTR)(LPCTSTR)pFragFile->GetName( i );
+		pItem.iItem		= m_wndFiles.InsertItem( &pItem );
+		m_wndFiles.SetItemText( pItem.iItem, 1, Settings.SmartVolume( pFragFile->GetLength( i ) ) );
+		if ( ! bCompleted )
+			m_wndFiles.SetColumnData( pItem.iItem, 3, pFragFile->GetPriority( i ) );
 	}
 
 	Update();
@@ -146,9 +143,13 @@ BOOL CTorrentFilesPage::OnApply()
 	{
 		if ( CComPtr< CFragmentedFile > pFragFile = pDownload->GetFile() )
 		{
-			for ( DWORD i = 0; i < pFragFile->GetCount(); ++i )
+			DWORD nCount = pFragFile ? pFragFile->GetCount() : 0;
+			if ( nCount > 1 )
 			{
-				pFragFile->SetPriority( i, m_wndFiles.GetColumnData( i, 3 ) );
+				for ( DWORD i = 0; i < nCount; ++i )
+				{
+					pFragFile->SetPriority( i, m_wndFiles.GetColumnData( i, 3 ) );
+				}
 			}
 		}
 	}

commit 21f9ab1f061a580ca86ab03bf5224b54ab2f66c0

Fixed torrent seeding broken in r9113.
git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@9116 3231ed4b-2112-0410-84e0-8c3944a40730

shareaza/DlgTorrentSeed.cpp
@@ -1,7 +1,7 @@
 //
 // DlgTorrentSeed.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2009.
+// Copyright (c) Shareaza Development Team, 2002-2012.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -168,6 +168,8 @@ void CTorrentSeedDlg::OnDownload()
 		EndDialog( IDOK );
 		return;
 	}
+
+	pDownload->PrepareFile();
 
 	if ( ( GetAsyncKeyState( VK_SHIFT ) & 0x8000 ) == 0 &&
 		! Network.IsWellConnected() )

shareaza/DownloadWithTorrent.cpp
@@ -413,7 +413,7 @@ BOOL CDownloadWithTorrent::SetTorrent(const CBTInfo* pTorrent)
 	// Re-link
 	DownloadGroups.Link( static_cast< CDownload* >( this ) );
 
-	OpenFile();
+	SetModified();
 
 	return TRUE;
 }