bda07b1fb80adaaa4f49ff5126f95c811d1819a9
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2012-10-05 15:59:36 +0000

    Fixed crash inside CUploadTransferBT::ServeRequests() function (probably when Shareaza tries to upload yet incomplete magnet-torrent).
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@9199 3231ed4b-2112-0410-84e0-8c3944a40730

--- shareaza/UploadTransferBT.cpp
+++ shareaza/UploadTransferBT.cpp
@@ -1,7 +1,7 @@
 //
 // UploadTransferBT.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2010.
+// Copyright (c) Shareaza Development Team, 2002-2012.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -324,7 +324,7 @@ BOOL CUploadTransferBT::ServeRequests()
 		m_oRequested.pop_front();
 	}
 	
-	if ( m_nLength != SIZE_UNKNOWN && m_nLength != 0 )
+	if ( m_nLength != SIZE_UNKNOWN && m_nLength != 0 && m_pDownload->m_pTorrent.m_nBlockSize != 0 )
 	{
 		if ( ! OpenFile() ) return FALSE;
 		
