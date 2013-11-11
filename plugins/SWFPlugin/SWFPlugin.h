db744e8e7958ffbc3156e76528b5fe81b43558e6
ivan <ivan386@yandex.ru>
2013-11-12 00:01:34 +0400

    New gitignore

--- plugins/SWFPlugin/SWFPlugin.h
+++ plugins/SWFPlugin/SWFPlugin.h
@@ -4,10 +4,10 @@
 
 
  /* File created by MIDL compiler version 7.00.0500 */
-/* at Thu Oct 24 01:06:04 2013
+/* at Mon Nov 11 23:06:39 2013
  */
 /* Compiler settings for .\SWFPlugin.idl:
-    Oicf, W1, Zp8, env=Win64 (32b run)
+    Oicf, W1, Zp8, env=Win32 (32b run)
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
