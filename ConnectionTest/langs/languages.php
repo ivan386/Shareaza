12bd1999259f57f88d50493383b0744d5d27956f
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2014-05-02 05:41:47 +0000

    Added Shareaza Connection Test php-script (c) jlh, 2009.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@9393 3231ed4b-2112-0410-84e0-8c3944a40730

---
+++ ConnectionTest/langs/languages.php
@@ -0,0 +1,55 @@
+<?php
+/*
+   Copyright (c) jlh, 2004 - 2008
+   This file is part of the Shareaza Connection Test
+
+   The Shareaza Connection Test is free software; you can redistribute
+   it and/or modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The Shareaza Connection Test is distributed in the hope that it will
+   be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Shareaza; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   Also visit http://www.gnu.org/
+*/
+
+/*
+   Shareaza Connection Test
+   this is the list of available languages.
+*/
+
+/*
+   the keys of the array are the ISO 639 code for the language and the
+   associated string is the name of the language in its native form.  the
+   language files are called 'lang-xx.php' where xx is the language code and
+   reside in the same path as this file.
+
+   this file is in UTF-8
+*/
+
+$lang_list = array(
+	'en' => "English",
+	'de' => "Deutsch",
+	'pl' => "Polski",
+	'nl' => "Nederlands",
+	'fr' => "Français",
+	'es' => "Español",
+	'it' => "Italiano",
+	'pt' => "Português",
+	'fi' => "Suomi",
+	'lt' => "Lietuviškai",
+	'he' => "עברית",
+	'zh' => "中文",
+	'tr' => "Türkçe",
+	'gr' => "Ελληνικά",
+	'ru' => "Русский",
+);
+
+?>
