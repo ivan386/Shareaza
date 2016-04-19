f137cb8a96bb1195196c1f8fe7f4c9a88b1698c8
raspopov <raspopov@3231ed4b-2112-0410-84e0-8c3944a40730>
2016-04-19 17:52:26 +0000

    Updated MiniUPnPc library to 1.9.20160209.
    git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@9626 3231ed4b-2112-0410-84e0-8c3944a40730

--- MiniUPnPc/miniupnpc/miniupnpc.h
+++ MiniUPnPc/miniupnpc/miniupnpc.h
@@ -1,8 +1,8 @@
-/* $Id: miniupnpc.h,v 1.48 2015/10/08 16:19:40 nanard Exp $ */
+/* $Id: miniupnpc.h,v 1.49 2016/01/24 17:24:36 nanard Exp $ */
 /* Project: miniupnp
  * http://miniupnp.free.fr/
  * Author: Thomas Bernard
- * Copyright (c) 2005-2015 Thomas Bernard
+ * Copyright (c) 2005-2016 Thomas Bernard
  * This software is subjects to the conditions detailed
  * in the LICENCE file provided within this distribution */
 #ifndef MINIUPNPC_H_INCLUDED
@@ -19,8 +19,8 @@
 #define UPNPDISCOVER_MEMORY_ERROR (-102)
 
 /* versions : */
-#define MINIUPNPC_VERSION	"1.9.20151008"
-#define MINIUPNPC_API_VERSION	15
+#define MINIUPNPC_VERSION	"1.9.20160209"
+#define MINIUPNPC_API_VERSION	16
 
 /* Source port:
    Using "1" as an alias for 1900 for backwards compatability
