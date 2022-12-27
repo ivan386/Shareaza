72cee30e731f076e267d180cca8d34abf47e6798
Salvatore Ansani <salvatoreansani@gmail.com>
2022-12-27 22:59:14 +0100

    Added support for external Definitions

--- shareaza/revision.h
+++ shareaza/revision.h
@@ -1,6 +1,16 @@
 #pragma once
 
-#ifndef BUILDFROMMSBUILD
-	#define __REVISION__		"Manually Built"
-	#define __REVISION_DATE__	"undefined"
-#endif // !BUILDFROMMSBUILD
+#define STRINGIZE2(x) #x 
+#define STRINGIZE(x) STRINGIZE2(x)
+
+#ifndef RevisionSHA
+#define __REVISION__		"BUILT MANUALLY"
+#else
+#define __REVISION__		RevisionSHA
+#endif
+
+#ifndef RevisionDate
+#define __REVISION_DATE__	"NOT AVAILABLE"
+#else
+#define __REVISION_DATE__	RevisionDate
+#endif
